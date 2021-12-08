1d9fc57f69cd3b7be8cf87ba69372bd4846954bc
Nick Young <youngnicholas228@gmail.com>
2021-12-08 12:02:35 -0500

    prettier

--- src/js/main.js
+++ src/js/main.js
@@ -1,45 +1,45 @@
 import * as THREE from 'three'
 import '/public/style.css'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { updateSize, animate, setupLighting, setupFog, setupPlane } from "./app"
+import { updateSize, animate, setupLighting, setupFog, setupPlane } from './app'
 import { setupGamepadAndListeners } from './movement'
 import { camOffsetX, camOffsetY, camOffsetZ } from './config'
 import Stats from '/../node_modules/stats.js/src/Stats.js'
 
 function main() {
-    // Listeners
-    window.addEventListener('resize', updateSizeOnWindowResize)
-    function updateSizeOnWindowResize() {
-        updateSize(renderer, camera)
-    }
+  // Listeners
+  window.addEventListener('resize', updateSizeOnWindowResize)
+  function updateSizeOnWindowResize() {
+    updateSize(renderer, camera)
+  }
 
-    // Statistics Tracking
-    const stats = new Stats()
-    stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
-    document.body.appendChild(stats.dom)
+  // Statistics Tracking
+  const stats = new Stats()
+  stats.showPanel(0) // 0: fps, 1: ms, 2: mb, 3+: custom
+  document.body.appendChild(stats.dom)
 
-    // Camera setup
-    const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, -10, 1000)
-    camera.position.set(camOffsetX, camOffsetY, camOffsetZ)
-    camera.lookAt(0, 0, 0)
+  // Camera setup
+  const camera = new THREE.OrthographicCamera(-1, 1, 1, -1, -10, 1000)
+  camera.position.set(camOffsetX, camOffsetY, camOffsetZ)
+  camera.lookAt(0, 0, 0)
 
-    // Renderer setup
-    const renderer = new THREE.WebGLRenderer({
-        canvas: document.querySelector('#cityscape'),
-    })
+  // Renderer setup
+  const renderer = new THREE.WebGLRenderer({
+    canvas: document.querySelector('#cityscape'),
+  })
 
-    // Controls setup
-    const orbitControls = new OrbitControls(camera, renderer.domElement)
+  // Controls setup
+  const orbitControls = new OrbitControls(camera, renderer.domElement)
 
-    // Scene setup
-    const scene = new THREE.Scene()
-    setupLighting(renderer, scene)
-    setupFog(scene)
-    setupPlane(scene)
+  // Scene setup
+  const scene = new THREE.Scene()
+  setupLighting(renderer, scene)
+  setupFog(scene)
+  setupPlane(scene)
 
-    // Start the loop.
-    setupGamepadAndListeners()
-    animate(stats, scene, renderer, camera, 0)
+  // Start the loop.
+  setupGamepadAndListeners()
+  animate(stats, scene, renderer, camera, 0)
 }
 
 main()
